2493a192cfca4cbcba7468c0a9f7e9727f360a07
cheyulin <yche@cse.ust.hk>
2017-02-20 09:55:48 +0800

    format and add git ignore

--- Javascript/study_webpack/webpack.config.js
+++ Javascript/study_webpack/webpack.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  entry: './index.js',
-  output: {
-    path: __dirname,
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loader: 'style!css!'
-      }
-    ]
-  }
+    entry: './index.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: 'style!css!'
+            }
+        ]
+    }
 }
